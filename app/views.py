4bc1b37cb040228259ab314a00fc71bab55afebc
AchyuthSiddha <achyuthsiddha43@gmail.com>
2023-06-09 20:05:07 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -20,6 +20,43 @@ class Product_Curd(APIView):
         PJD=ProductSerializer(SQS,many=True)
         return Response(PJD.data)
     
+    def post(self,request):
+        PMSD=ProductSerializer(data=request.data)### collect the date into models and giving into model seralizer and convert data
+        if PMSD.is_valid():# is_valid is valid the data whatever the data will approprate giving or not
+            PMSD.save()
+            return Response({'message':'product is created:'})
+        return Response({'Failed':'product is failed'})
     
+    def put(self,request):
+        # PID=request.get['PID']
+        # UO=Product.objects.get(PID=PID)############ we have to write on based on object we have update the date
+        id=request.data['id']
+        UO=Product.objects.get(id=id)
+        PUOD=ProductSerializer(UO,data=request.data)
+        if PUOD.is_valid():
+            PUOD.save()
+            return Response({'message':'product is Update'})
+        return Response({'Failed':'product is not Update'})
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     
         
